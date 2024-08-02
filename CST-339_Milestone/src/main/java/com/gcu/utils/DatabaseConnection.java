e06fdcfad94173fbf9f48badf3c0af972167efa9
sarisadi1 <90878986+sarisadi1@users.noreply.github.com>
2024-08-02 12:42:07 -0700

    Add files via upload

---
+++ CST-339_Milestone/src/main/java/com/gcu/utils/DatabaseConnection.java
@@ -0,0 +1,34 @@
+package com.gcu.utils;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DatabaseConnection {
+
+    private static DatabaseConnection instance;
+    private Connection connection;
+
+    private DatabaseConnection() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/springBootApplication";
+            String username = "root";
+            String password = "16789";
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println(" connected ");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static synchronized DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+}
